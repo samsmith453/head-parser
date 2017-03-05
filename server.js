e7a1f84fdd231a33721c2f92503342ff539e9ceb
Sam <samsmith453@gmail.com>
2017-03-05 10:22:45 +0000

    finished

--- server.js
+++ server.js
@@ -2,7 +2,20 @@ var express =require("express");
 var app = express();
 
 app.get("/", function(req, res){
-    res.send("Hello");
+    
+    var agent = req.headers["user-agent"];
+    var reg = /\(.+?\)/;
+    var info = agent.match(reg);
+    
+    var obj = {
+        "IP Address": req.headers["x-forwarded-for"],
+        "language": req.headers["accept-language"],
+        "user": info[0]
+    }
+    
+    res.send(obj);
 });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, function(){
+    console.log("We're online");
+});
